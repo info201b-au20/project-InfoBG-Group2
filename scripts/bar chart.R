ef824b4beb3d086158d287bcf9bc3a21e996252f
Ellie Stegman <stegme@uw.edu>
2020-11-12 09:50:42 -0800

    index update

--- scripts/bar chart.R
+++ scripts/bar chart.R
@@ -2,13 +2,13 @@
 
 library(tidyverse)
 library(ggplot2)
-shootings <- read.csv("https://media.githubusercontent.com/media/info201b-au20/project-InfoBG-Group2/gh-pages/data/USPoliceShootings.csv")
+shootings <- read.csv("C:/Users/admin/Documents/Info201/Data/USPoliceShootings.csv")
 
 by_race <- shootings %>% 
   group_by(race) %>%
   summarise(race_count = n())
 
-ggplot(data = by_race) +
+shooting_by_race <- ggplot(data = by_race) +
   geom_col(mapping = aes(x = race, y = race_count)) +
   labs(x = "Race", y = "Number Killed", title = "US Police Shooting Victims by Race")
 
