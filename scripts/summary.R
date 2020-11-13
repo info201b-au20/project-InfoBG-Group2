3288a1755f3aeaeba8302e2ad6ab9dd52dea3bb7
heceoz <61093082+heceoz@users.noreply.github.com>
2020-11-12 23:16:06 -0800

    Made into a list

--- scripts/summary.R
+++ scripts/summary.R
@@ -2,35 +2,36 @@ library(tidyverse)
 library(dplyr)
 library(stringr)
 
-shootings <- read.csv("USPoliceShootings.csv")
-rows <- nrow(shootings)
-cols <- ncol(shootings)
+shootings <- read.csv("shootings.csv")
+summary_info <- list()
+summary_info$rows <- nrow(shootings)
+summary_info$cols <- ncol(shootings)
 
 # Location
-num_wa <- sum(str_detect(shootings$state, "WA"))
-num_ca <- sum(str_detect(shootings$state, "CA"))
-num_ms <- sum(str_detect(shootings$state, "MS"))
-num_tx <- sum(str_detect(shootings$state, "TX"))
+summary_info$num_wa <- sum(str_detect(shootings$state, "WA"))
+summary_info$num_ca <- sum(str_detect(shootings$state, "CA"))
+summary_info$num_ms <- sum(str_detect(shootings$state, "MS"))
+summary_info$num_tx <- sum(str_detect(shootings$state, "TX"))
 
 # Average Age
 mean_age <- mean(shootings$age)
 
 # Racial Groups
-num_white <- sum(str_detect(shootings$race, "White"))
-num_asian <- sum(str_detect(shootings$race, "Asian"))
-num_black <- sum(str_detect(shootings$race, "Black"))
-num_hispanic <- sum(str_detect(shootings$race, "Hispanic"))
+summary_info$num_white <- sum(str_detect(shootings$race, "White"))
+summary_info$num_asian <- sum(str_detect(shootings$race, "Asian"))
+summary_info$num_black <- sum(str_detect(shootings$race, "Black"))
+summary_info$num_hispanic <- sum(str_detect(shootings$race, "Hispanic"))
 
 # Date
 dates <- as.Date(shootings[['date']])
-latest <- max(dates)
-earliest <- min(dates)
-in_2020 <- length(dates[format(dates, "%Y") == "2020"])
-in_2019 <- length(dates[format(dates, "%Y") == "2019"])
-in_2018 <- length(dates[format(dates, "%Y") == "2018"])
-in_2017 <- length(dates[format(dates, "%Y") == "2017"])
-in_2016 <- length(dates[format(dates, "%Y") == "2016"])
-in_2015 <- length(dates[format(dates, "%Y") == "2015"])
+summary_info$latest <- max(dates)
+summary_info$earliest <- min(dates)
+summary_info$in_2020 <- length(dates[format(dates, "%Y") == "2020"])
+summary_info$in_2019 <- length(dates[format(dates, "%Y") == "2019"])
+summary_info$in_2018 <- length(dates[format(dates, "%Y") == "2018"])
+summary_info$in_2017 <- length(dates[format(dates, "%Y") == "2017"])
+summary_info$in_2016 <- length(dates[format(dates, "%Y") == "2016"])
+summary_info$in_2015 <- length(dates[format(dates, "%Y") == "2015"])
 
 # Mental Illness
-mentall_ill <- filter(shootings, signs_of_mental_illness == "True")
+summary_info$mentall_ill <- filter(shootings, signs_of_mental_illness == "True")
