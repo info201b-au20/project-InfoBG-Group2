8a3dc1e258c246211d6e7a256214dedf9fdb9c73
heceoz <61093082+heceoz@users.noreply.github.com>
2020-12-07 10:29:14 -0800

    Added intro panel

--- app_ui.R
+++ app_ui.R
@@ -1,3 +1,21 @@
+intro_panel <- tabPanel(
+  "Introduction",
+  
+  titlePanel("Gun Violence in the US"),
+  
+  sidebarPanel(
+    h4("About the Data"),
+    p(em("On this project, we focused on gun violence in the US. For the analysis we
+      primarily used 'US Police Shootings' data from Kaggle.com which was 
+      collected by AhsenNazir, a data analyst. The data is about police shootings in the United 
+      States across different states. The data also involves details about a 
+      race, whether the person was armed, if the victim had a mental illness, etc. 
+      It contains 5924 observations and 15 features.
+      "))
+  ),
+  main_content
+)
+
 y_input <- selectInput(
   inputId = "y_input",
   label = "Choose a Year",
@@ -14,10 +32,7 @@ y_input <- selectInput(
 ui <- fluidPage(
   navbarPage(
     "US Gun Violence & Police Shooting Report",
-    tabPanel(
-      title = "Introduction",
-    ),
-    
+    intro_panel,
     tabPanel(
       title = "Map", 
       titlePanel("Gun Violence Trends in the United States"),
